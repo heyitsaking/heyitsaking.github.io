8ecb36fba687e4696782e3efb052c2cb0413a128
heyitsaking <rey.arturo95@gmail.com>
2020-02-11 11:20:15 -0500

    profile base

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ class App extends React.Component {
   render() {
       return (
           <body>
-              <div class="jumbotron">
+              <div class={`jumbotron`}>
                   <div class={`sheet bg-black o-40`}></div>
                   <div class={`main-text`}>
-                      <header className="resize tc flex flex-column justify-center items-center">
-                          <h1 className="f-5-ns f1 ttu fw9 white-90 mv3">
+                      <header class={`resize tc flex flex-column justify-center items-center`}>
+                          <h1 class={`f-5-ns f1 ttu fw9 white-90 mv3`}>
                               Arturo A. King
                           </h1>
-                          <h2 className="f-1-ns f2 fw3 white-80 mt0 lh-copy">
+                          <h2 class={`f-1-ns f2 fw3 white-80 mt0 lh-copy`}>
                               B.S. in Computer Engineering
                           </h2>
                       </header>
                   </div>
               </div>
-              <InfoPanel bgColor={`bg-navy`} cardTitle={`About me...`}/>
-              <InfoPanel bgColor={`bg-orange`} cardTitle={`Experience`}/>
+              <InfoPanel bgColor={`bg-navy`} cardTitle={`About me...`} cardType={`profile`}/>
+              <InfoPanel bgColor={`bg-orange`} cardTitle={`Experience`} cardType={`list`}/>
           </body>
       );
   }
--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import './InfoPanel.css';
 
-const InfoPanel = ({bgColor, cardTitle}) => {
+const InfoPanel = ({bgColor, cardTitle, cardType}) => {
     return (
         <div className={`flex justify-center info-panel ${bgColor}`}>
-            <article className="hidden mt4 mh3 mb4">
-                <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">{cardTitle}</h1>
-                <div className="pa3 bt b--black-10 bg-white br--bottom br3">
-                    <p className="f6 f5-ns lh-copy mw8">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                        vero eos et accusam et justo duo dolores et ea rebum.
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                        vero eos et accusam et justo duo dolores et ea rebum.
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                        vero eos et accusam et justo duo dolores et ea rebum.
-                    </p>
-                </div>
-            </article>
+            {cardType === 'profile' ?
+                <article className="w-100 flex items-center justify-around">
+                    <div className="white">
+                        Info
+                    </div>
+                    <div className="white">
+                        <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+                            <div className="tc">
+                                <img src="./profile.jpg"
+                                     className="br-100 h4 w4 dib black ba b--black-05 pa2"
+                                     title="Photo of Arturo King"
+                                     alt="Arturo A King"/>
+                                <h1 className="f3 black mb2">Arturo A. King</h1>
+                            </div>
+                        </article>
+                    </div>
+                    <div className="white">
+                        More info
+                    </div>
+                </article>
+            : (
+                <article className="hidden mt4 mh3 mb4">
+                    <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">{cardTitle}</h1>
+                    <div className="pa3 bt b--black-10 bg-white br--bottom br3">
+                        <p className="f6 f5-ns lh-copy mw8">
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                            vero eos et accusam et justo duo dolores et ea rebum.
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                            vero eos et accusam et justo duo dolores et ea rebum.
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                            vero eos et accusam et justo duo dolores et ea rebum.
+                        </p>
+                    </div>
+                </article>
+            )}
         </div>
     )
 }
