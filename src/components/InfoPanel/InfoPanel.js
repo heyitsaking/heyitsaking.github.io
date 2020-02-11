42236333277a3384d86999261f4ad23e8bd6707b
heyitsaking <rey.arturo95@gmail.com>
2020-02-11 15:06:57 -0500

    new responsive profile base

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -5,27 +5,28 @@ const InfoPanel = ({bgColor, cardTitle, cardType}) => {
     return (
         <div className={`flex justify-center info-panel ${bgColor}`}>
             {cardType === 'profile' ?
-                <article className="w-100 flex items-center justify-around">
-                    <div className="white">
-                        Info
-                    </div>
-                    <div className="white">
-                        <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
-                            <div className="tc">
-                                <img src="./profile.jpg"
-                                     className="br-100 h4 w4 dib black ba b--black-05 pa2"
-                                     title="Photo of Arturo King"
-                                     alt="Arturo A King"/>
-                                <h1 className="f3 black mb2">Arturo A. King</h1>
+                <div className="mw9 w-100 center ph3-ns flex justify-center items-center">
+                    <div className="cf ph2-ns w-100 justify-between-l">
+                        <div className="fl w-100 w-third-l pa2">
+                            <div className="outline bg-white pv4">
+                                <h2>
+                                    About me...
+                                </h2>
+                                <p>
+
+                                </p>
                             </div>
-                        </article>
+                        </div>
+                        <div className="fl w-100 w-third-l pa2">
+                            <div className="outline bg-white pv4"></div>
+                        </div>
+                        <div className="fl w-100 w-third-l pa2">
+                            <div className="outline bg-white pv4"></div>
+                        </div>
                     </div>
-                    <div className="white">
-                        More info
-                    </div>
-                </article>
+                </div>
             : (
-                <article className="hidden mt4 mh3 mb4">
+                <article className="flex flex-column justify-center hidden mt4 mh3 mb4">
                     <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">{cardTitle}</h1>
                     <div className="pa3 bt b--black-10 bg-white br--bottom br3">
                         <p className="f6 f5-ns lh-copy mw8">
