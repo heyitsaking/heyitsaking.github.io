00ab446dc0759aecd81e132d77dbf44063efab75
heyitsaking <rey.arturo95@gmail.com>
2020-02-24 09:59:07 -0500

    nib info panel mostly done

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ class App extends React.Component {
     }
 
     onClick = () => {
-        console.log('Route changed')
-        this.setState({route: 'cardi'});
+        if(this.state.route == 'main') {
+            this.setState({route: 'cardi'});
+        } else {
+            this.setState({route: 'main'});
+        }
     };
 
     render() {
@@ -21,23 +24,22 @@ class App extends React.Component {
             <div>
                 <div className={(route === 'main') ? 'dn' : 'cardiInfo db bg-black-30 h-100 w-100 flex items-center justify-center'}>
                     <div className="dt center pv1-m pv1-ns ph0-ns ph2 bg-white mw8-l w-80 ba b--orange bw2">
-                        <div className={`flex flex-row-reverse pt1 pr1 h2`}>
-                            <button className={`bg-transparent f5`}>X</button>
+                        <div className={`flex flex-row-reverse h1 mv1 mr1-ns`}>
+                            <a onClick={this.onClick} className={`outline w1 h1 bg-transparent f5 flex items-center justify-center courier`}>X</a>
                         </div>
                         <div className={`pr1-ns`}>
                             <div className="db dtc-ns v-mid-ns pl1-ns pa1">
                                 <img src={require(`./nib.jpg`)} alt="A bright blue sky"
                                      className="w-100 mw7 w5-ns"/>
                             </div>
-                            <div className="db dtc-ns v-mid ph2 pr2-ns pl1-ns">
-                                <p className="lh-copy tj navy avenir f7-m">
-                                    {`This is Cardi. We initially fostered her and her two siblings, Nicki and Chance (the
-                                    Humane Society likes naming their litters with a theme), when they were two weeks
-                                    old. Sadly, Nicki and Chance were already sick when we began fostering and passed
-                                    away :( We then decided to adopt Cardi, and while we don't really call her by that
-                                    name, we decided to keep it in memory of her siblings.`}
+                            <div className="db dtc-ns v-mid ph2 pr2-ns pl1-ns avenir f4-l f5-m">
+                                <p className="lh-copy tj navy">
+                                    This is Cardi. We initially fostered her when she was two weeks old. We then
+                                    decided to keep Cardi when she became adoptable, but we mostly call her Nibbles.
                                 </p>
-                                Follow her adventures on Instagram! <a href={`https://www.instagram.com/cardic_calico/?igshid=h7riz1jen7v6`}>@cardic_calico</a>
+                                <p>
+                                    Follow her adventures on Instagram! <a href={`https://www.instagram.com/cardic_calico/?igshid=h7riz1jen7v6`}>@cardic_calico</a>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -51,8 +53,8 @@ class App extends React.Component {
                                   <h1 class={`f-5-ns f1 ttu fw9 white-90 mv3`}>
                                       Arturo A. King
                                   </h1>
-                                  <h2 class={`f-1-ns f2 fw3 white-80 mt0 lh-copy`}>
-                                      B.S. in Computer Engineering
+                                  <h2 class={`f-1-ns f2 fw3 white-80 bg-black-10 mt0 lh-copy outline`}>
+                                      B.E. in Computer Engineering
                                   </h2>
                               </header>
                           </div>
