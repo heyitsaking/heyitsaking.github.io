4719e5932774f7646f67ea72f2baf1133edc6be6
heyitsaking <rey.arturo95@gmail.com>
2020-03-16 15:36:39 -0400

    changed responsiveness and styling of profile panel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 import InfoPanel from './components/InfoPanel/InfoPanel';
 import NibDisplay from './components/NibDisplay/NibDisplay';
 import ProfilePanel from "./components/ProfilePanel/ProfilePanel";
--- src/components/ProfilePanel/ProfilePanel.js
+++ src/components/ProfilePanel/ProfilePanel.js
@@ -1,27 +1,23 @@
 import React from 'react';
 
 const ProfilePanel = ({bgColor, onLinkClick}) => {
+    let cardiLink = <span onClick={onLinkClick} className={`underline white hover-orange`}>Cardi</span>;
+
     return (
         <div className={`flex justify-center info-panel ${bgColor}`}>
-            <div className="flex flex-row-l flex-column items-center justify-around-ns ph3-ns mv3 avenir">
-                <div className="dtc-ns tc w-33-l w-100 h-100-ns">
-                    <div className="flex items-center white bg-white-10 ph2-ns ph4 h-inherit f3 pc0-ns pv2">
+            <div className="flex flex-row-ns items-center justify-between-ns ph5 mv3 mw8-ns w-75-m avenir">
+                <div className="flex items-center w-75-l w-auto h-100-ns pv4-ns">
+                    <div className="white tr-l tc f2-ns f4 fw3 pr3 pc0-ns pv5-ns pv4 h-100">
                         Aspiring developer with front-end experience using React and other Javascript
                         frameworks. Looking for opportunities to better myself in both front-end
-                        and full-stack development.
+                        and full-stack development. Currently residing in Jacksonville, FL, with my wife Mary Grayson
+                        and our cat, {cardiLink} (a.k.a Nibbles).
                     </div>
                 </div>
-                <div className="dtc-l tc dn pv4 w-33-ns mw5">
+                <div className="flex-l items-center tc dn h-100 pv4">
                     <img src={require('./profile.jpg')}
-                         alt={`Arturo A. King`}/>
-                </div>
-                <div className="dtc-ns tc w-33-l w-100 h-100-ns">
-                    <div className="flex items-center white bg-white-10 ph2-ns pb0-ns ph4 pb4 h-inherit">
-                        <div className={`f3 ws-normal pt0-ns pt3`}>
-                            Currently residing in Jacksonville, FL, with my wife Mary Grayson
-                            and our cat, <a href={`/#`} onClick={onLinkClick} className={`underline white hover-orange`}>Cardi</a> (a.k.a Nibbles).
-                        </div>
-                    </div>
+                         alt={`Arturo A. King`}
+                         className={`mw5`}/>
                 </div>
             </div>
         </div>
