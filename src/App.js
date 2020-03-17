290bd69e4c1093b32ce04109f560e1bc0e2b4c7c
heyitsaking <rey.arturo95@gmail.com>
2020-03-17 11:29:33 -0400

    Cleaned main App component, changed components for reusability

--- src/App.js
+++ src/App.js
@@ -27,38 +27,37 @@ class App extends React.Component {
                 <Modal isOpen={showModal} className={'h-100 w-100 flex items-center justify-center'}>
                     <NibDisplay onClick={() => {this.handleModal()}}/>
                 </Modal>
-                <div className={`base`}>
-                    <div>
-                        <div className={`jumbotron`}>
-                            <div className={`main-text bg-black-40`}>
-                                <header className={`resize tc flex flex-column justify-center items-center`}>
-                                    <h1 className={`f1-ns f2 ttu fw9 white-90 mv3`}>
-                                        Arturo A. King
-                                    </h1>
-                                    <h2 className={`f2-ns f3 fw7 white-90 mt0 lh-copy`}>
-                                        B.E. in Computer Engineering
-                                    </h2>
-                                </header>
+                <div>
+                    <div className={`jumbotron`}>
+                        <div className={`main-text bg-black-40`}>
+                            <header className={`resize tc flex flex-column justify-center items-center`}>
+                                <h1 className={`f1-ns f2 ttu fw9 white-90 mv3`}>
+                                    Arturo A. King
+                                </h1>
+                                <h2 className={`f2-ns f3 fw7 white-90 mt0 lh-copy`}>
+                                    B.E. in Computer Engineering
+                                </h2>
+                            </header>
+                        </div>
+                    </div>
+                    <InfoPanel bgColor={`bg-navy`}>
+                        <div className="flex items-center w-75-l w-auto h-100-ns pv4-ns">
+                            <div className="white tr-l tc f2-ns f4 fw3 pr3 pc0-ns pv5-ns pv4 h-100">
+                                Aspiring developer with front-end experience using React and other Javascript
+                                frameworks. Looking for opportunities to better myself in both front-end
+                                and full-stack development. Currently residing in Jacksonville, FL, with my wife Mary Grayson
+                                and our cat, {cardiLink} (a.k.a Nibbles).
                             </div>
                         </div>
-                        <InfoPanel bgColor={`bg-navy`}>
-                            <div className="flex flex-row-ns items-center justify-between-ns ph5 mv3 mw8-ns w-75-m avenir">
-                                <div className="flex items-center w-75-l w-auto h-100-ns pv4-ns">
-                                    <div className="white tr-l tc f2-ns f4 fw3 pr3 pc0-ns pv5-ns pv4 h-100">
-                                        Aspiring developer with front-end experience using React and other Javascript
-                                        frameworks. Looking for opportunities to better myself in both front-end
-                                        and full-stack development. Currently residing in Jacksonville, FL, with my wife Mary Grayson
-                                        and our cat, {cardiLink} (a.k.a Nibbles).
-                                    </div>
-                                </div>
-                                <div className="flex-l items-center tc dn h-100 pv4">
-                                    <img src={require('./profile.jpg')}
-                                        alt={`Arturo A. King`}
-                                        className={`mw5`}/>
-                                </div>
-                            </div>
-                        </InfoPanel>
-                    </div>
+                        <div className="flex-l items-center tc dn h-100 pv4">
+                            <img src={require('./profile.jpg')}
+                                alt={`Arturo A. King`}
+                                className={`mw5`}/>
+                        </div>
+                    </InfoPanel>
+                    <InfoPanel bgColor={`bg-orange`}>
+
+                    </InfoPanel>
                 </div>
             </div>
         );
--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -3,7 +3,9 @@ import React from 'react';
 function InfoPanel (props) {
     return (
         <div className={`flex justify-center info-panel ${props.bgColor}`}>
-            {props.children}
+            <div className="flex flex-row-ns items-center justify-between-ns ph5 mv3 mw8-ns w-75-m avenir">
+                {props.children}
+            </div>
         </div>
     )
 };
