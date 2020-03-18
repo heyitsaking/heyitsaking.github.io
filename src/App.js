a635c974aaba4033be684a0e31a659880381d913
heyitsaking <rey.arturo95@gmail.com>
2020-03-18 14:24:38 -0400

    Footer added

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
     }
 
     render() {
-        let cardiLink = <span onClick={this.handleModal} className={`underline white hover-orange`}>Cardi</span>;
+        let cardiLink = <span onClick={this.handleModal} className={`underline light-white hover-orange`}>Cardi</span>;
         const { showModal } = this.state;
 
         return (
@@ -45,7 +45,7 @@ class App extends React.Component {
                     <InfoPanel title={`About me...`} bg={`navy`}>
                         <div className={`flex justify-between-ns mw8-ns w-75-m ph5`}>
                             <div className="w-75-l w-auto pt2-ns pb4-ns">
-                                <div className="near-white tr-l tc f2-ns f4 fw3 pr3 pc0-ns pv2-ns pv4">
+                                <div className="moon-gray tr-l tc f2-ns f4 fw3 pr3 pc0-ns pv2-ns pv4">
                                     Aspiring developer with front-end experience using React and other Javascript
                                     frameworks. Looking for opportunities to better myself in both front-end
                                     and full-stack development. Currently residing in Jacksonville, FL, with my wife Mary Grayson
@@ -61,7 +61,7 @@ class App extends React.Component {
                     </InfoPanel>
                     <InfoPanel title={`Experience`} bg={`orange`}>
                         <div className="flex justify-center w-100 ph3-ns">
-                            <div className="flex-ns justify-around-ns cf w-100-ns mw7 ph2-ns ph2">
+                            <div className="flex-ns justify-around-ns fw3 cf w-100-ns mw7 ph2-ns ph2">
                                 {data.experience.map((level, i) => {
                                     return (
                                         <SkillPanel level={level.proficiency} skills={level.skills} key={i}/>
@@ -71,6 +71,25 @@ class App extends React.Component {
                         </div>
                     </InfoPanel>
                 </div>
+                <footer className={`code`} style={{'background-image':'linear-gradient(to right, #F4F4F4, rgba(204, 204, 204, .7))'}}>
+                    <div className={`flex flex-row ph5 pt3 f5`}>
+                        <img src={require(`./images/crown.png`)}
+                            alt={`Crown Icon`}
+                            className={`mw4`}/>
+                        <div className={`flex items-center`}>
+                            <ul>
+                                Contact Information:
+                                <li className={`ml5 pl3`}>
+                                    Email: arturo.a.king.p@gmail.com
+                                </li>
+                                <li className={`ml5 pl3`}>
+                                    Mobile Phone: (404) 984-4477
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={`w-100 tc f4 pb2`}>UI made with React</div>
+                </footer>
             </div>
         );
     }
