222feaad312e77e594d1b38cbd246dec8fc54792
heyitsaking <rey.arturo95@gmail.com>
2020-03-18 12:43:17 -0400

    Updates

---
+++ precache-manifest.9400f112721beb15bd319e47e2db11c0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdcf1715ff95e40fc113c65f2de496b",
+    "url": "http//:heyitsaking.github.io/my-site/index.html"
+  },
+  {
+    "revision": "e5a2d497f2dff260c8db",
+    "url": "http//:heyitsaking.github.io/my-site/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d108259abfd9a17d1407",
+    "url": "http//:heyitsaking.github.io/my-site/static/css/main.40ebf12a.chunk.css"
+  },
+  {
+    "revision": "e5a2d497f2dff260c8db",
+    "url": "http//:heyitsaking.github.io/my-site/static/js/2.b43c80ad.chunk.js"
+  },
+  {
+    "revision": "192201c36557064462a543b746126438",
+    "url": "http//:heyitsaking.github.io/my-site/static/js/2.b43c80ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d108259abfd9a17d1407",
+    "url": "http//:heyitsaking.github.io/my-site/static/js/main.9024e9c7.chunk.js"
+  },
+  {
+    "revision": "7b01905718e98edaad15",
+    "url": "http//:heyitsaking.github.io/my-site/static/js/runtime-main.8c652368.js"
+  },
+  {
+    "revision": "ce79e869bc0a16ee0e0de0ac0b58085f",
+    "url": "http//:heyitsaking.github.io/my-site/static/media/3star.ce79e869.png"
+  },
+  {
+    "revision": "72430720f9b6d7bd32d7a0d800600e25",
+    "url": "http//:heyitsaking.github.io/my-site/static/media/4star.72430720.png"
+  },
+  {
+    "revision": "87017da603b1208e8d33ac3937716c38",
+    "url": "http//:heyitsaking.github.io/my-site/static/media/5star.87017da6.png"
+  },
+  {
+    "revision": "5fca4f204054dee6a35089912e70da84",
+    "url": "http//:heyitsaking.github.io/my-site/static/media/auburn.5fca4f20.jpg"
+  },
+  {
+    "revision": "5b45f625697339f30e238fe8d7e36ac2",
+    "url": "http//:heyitsaking.github.io/my-site/static/media/nib.5b45f625.jpg"
+  },
+  {
+    "revision": "b143a1f410572bff4b2c374930c54664",
+    "url": "http//:heyitsaking.github.io/my-site/static/media/profile.b143a1f4.jpg"
+  }
+]);
